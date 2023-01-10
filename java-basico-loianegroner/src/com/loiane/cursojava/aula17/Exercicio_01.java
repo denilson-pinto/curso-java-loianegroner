eb735bb074fb5de811ea1918770bd7511d486ef4
denilson-pinto <denilsonpinto89@gmail.com>
2023-01-10 11:38:32 -0300

    Aula 17: exercício 1

--- java-basico-loianegroner/src/com/loiane/cursojava/aula17/Exercicio_01.java
+++ java-basico-loianegroner/src/com/loiane/cursojava/aula17/Exercicio_01.java
@@ -6,11 +6,30 @@ package com.loiane.cursojava.aula17;
 	usuário informe um valor válido.
 */
 
+import java.util.Scanner;
+
 public class Exercicio_01 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		boolean ehValida = false;
+		
+		Scanner scan = new Scanner(System.in);
+		
+		while(!ehValida) {
+			
+			System.out.println("Informe uma nota entre 0-Zero e 10-Dez:");
+			double nota = scan.nextDouble();
+			
+			if(nota >= 0 && nota <= 10) {
+				ehValida = true;
+				System.out.println("Nota informada: " + nota);
+			} else {
+				System.out.println("Valor inválido!");
+			}
+			
+		}	
+		
 	}
 
 }
