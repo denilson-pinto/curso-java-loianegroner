bd86066af09fe8ba64ce2622a46c781ad65827e3
denilson-pinto <denilsonpinto89@gmail.com>
2023-01-12 17:50:28 -0300

    Aula 17: exercício 15

--- java-basico-loianegroner/src/com/loiane/cursojava/aula17/Exercicio_15.java
+++ java-basico-loianegroner/src/com/loiane/cursojava/aula17/Exercicio_15.java
@@ -5,11 +5,39 @@ package com.loiane.cursojava.aula17;
   Faça um programa capaz de gerar a série até o n−ésimo termo.
 */
 
+import java.util.Scanner;
+
 public class Exercicio_15 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Informe o n-ésimo termo da série de Fibonacci:");
+		int numero = scan.nextInt();
+		
+		int primeiro = 1;
+		int segundo = 1;
+		int proximo;
+		
+		if(numero < 2) {
+			System.out.println(numero);
+		} else {
+			
+			System.out.print(primeiro + " " + segundo + " ");
+			
+			for (int i = 3; i <= numero; i++) {
+
+				proximo = primeiro + segundo;
+				primeiro = segundo;
+				segundo = proximo;
+
+				System.out.print(proximo + " ");
+				
+			}
+		}
+			
+		
 	}
 
 }
