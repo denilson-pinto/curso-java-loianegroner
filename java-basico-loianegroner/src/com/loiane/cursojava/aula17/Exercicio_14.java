04675542b3338c154b50d351699cc736b8ad438e
denilson-pinto <denilsonpinto89@gmail.com>
2023-01-12 00:47:55 -0300

    Aula 17: exercício 14

--- java-basico-loianegroner/src/com/loiane/cursojava/aula17/Exercicio_14.java
+++ java-basico-loianegroner/src/com/loiane/cursojava/aula17/Exercicio_14.java
@@ -2,14 +2,32 @@ package com.loiane.cursojava.aula17;
 
 /*
   Faça um programa que peça 10 números inteiros, calcule e mostre a
-  quantidade de números pares e a quantidade de números impares.
+  quantidade de números pares e a quantidade de números ímpares.
 */
+
+import java.util.Scanner;
 
 public class Exercicio_14 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Scanner scan = new Scanner(System.in);
+		
+		int numero;
+		int totalPares = 0;
+		int totalImpares = 0;
+		
+		for(int i = 0; i < 10; i++) {
+			System.out.println("Informe um número:");
+			numero = scan.nextInt();
+			
+			if(numero % 2 == 0) totalPares++;
+			else totalImpares++;
+		}
+		
+		System.out.println("Quantidade de números pares: " + totalPares);
+		System.out.println("Quantidade de números ímpares: " + totalImpares);
+		
 	}
 
 }
