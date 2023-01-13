b6fa1d848703fd606cac76794ecd7893fa95190f
denilson-pinto <denilsonpinto89@gmail.com>
2023-01-13 00:30:45 -0300

    Aula 17: exercício 16

--- java-basico-loianegroner/src/com/loiane/cursojava/aula17/Exercicio_16.java
+++ java-basico-loianegroner/src/com/loiane/cursojava/aula17/Exercicio_16.java
@@ -8,8 +8,22 @@ package com.loiane.cursojava.aula17;
 public class Exercicio_16 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		int primeiro = 1;
+		int segundo = 1;
+		int proximo = 0;
+		
+		System.out.print(primeiro + " " + segundo + " ");
+		
+		while(proximo <= 500) {
+			
+			proximo = primeiro + segundo;
+			primeiro = segundo;
+			segundo = proximo;
 
+			System.out.print(proximo + " ");
+			
+		}
 	}
 
 }
