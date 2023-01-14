dcc10b00a735e04de4f2a0e934a92a5ac6478ec7
denilson-pinto <denilsonpinto89@gmail.com>
2023-01-14 19:13:42 -0300

    Aula 17: exercício 20

--- java-basico-loianegroner/src/com/loiane/cursojava/aula17/Exercicio_20.java
+++ java-basico-loianegroner/src/com/loiane/cursojava/aula17/Exercicio_20.java
@@ -7,11 +7,43 @@ package com.loiane.cursojava.aula17;
   ou idosa, conforme a média calculada.
 */
 
+import java.util.Scanner;
+
 public class Exercicio_20 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Informe quantos alunos tem a turma:");
+		int totalAlunos = scan.nextInt();
+		
+		System.out.println("------------------------------------");
+		
+		int idade;
+		int somaIdade = 0;
+		double mediaIdade;
+		
+		for(int i = 1; i <= totalAlunos; i++) {
+			System.out.println("Informe a idade do " + i + "º aluno:");
+			idade = scan.nextInt();
+			
+			somaIdade += idade;
+		}
+		
+		System.out.println("----------------------------------------");
+		
+		mediaIdade = somaIdade / totalAlunos;
+		
+		System.out.print("Média de idade: " + mediaIdade);
+		
+		if(mediaIdade >= 0 && mediaIdade <= 25) {
+			System.out.println(" - Turma de Jovens.");
+		} else if(mediaIdade >= 26 && mediaIdade <= 60) {
+			System.out.println(" - Turma de Adultos.");
+		} else if(mediaIdade > 60) {
+			System.out.println(" - Turma de Idosos.");
+		}
 	}
 
 }
