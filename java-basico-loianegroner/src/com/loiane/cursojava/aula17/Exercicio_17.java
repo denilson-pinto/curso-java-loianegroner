08169426a817222d2e22982df027296f3c3b3023
denilson-pinto <denilsonpinto89@gmail.com>
2023-01-13 22:29:52 -0300

    Aula 17: exercício 17

--- java-basico-loianegroner/src/com/loiane/cursojava/aula17/Exercicio_17.java
+++ java-basico-loianegroner/src/com/loiane/cursojava/aula17/Exercicio_17.java
@@ -5,11 +5,28 @@ package com.loiane.cursojava.aula17;
   fornecido pelo usuário. Ex.: 5!= 5.4.3.2.1 = 120.
 */
 
+import java.util.Scanner;
+
 public class Exercicio_17 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Informe um número para o cálculo do fatorial:");
+		int numero = scan.nextInt();
+		
+		System.out.println("\nFatorial de " + numero + " (" + numero + "!):");
+		
+		int fatorial = 1;
+		
+		for(int i = numero; i > 0; i--) {
+			fatorial *= i;
+			System.out.print(i + " ");
+		}
+		
+		System.out.println("\nResultado: " + fatorial);
+		
 	}
 
 }
