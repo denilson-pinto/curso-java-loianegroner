0cf80610d8b56cc77ed5a3cb039d4646548b48e7
denilson-pinto <denilsonpinto89@gmail.com>
2023-01-14 22:19:57 -0300

    Aula 17: exercício 21

--- java-basico-loianegroner/src/com/loiane/cursojava/aula17/Exercicio_21.java
+++ java-basico-loianegroner/src/com/loiane/cursojava/aula17/Exercicio_21.java
@@ -6,11 +6,55 @@ package com.loiane.cursojava.aula17;
   cada turma. As turmas não podem ter mais de 40 alunos.
 */
 
+import java.util.Scanner;
+
 public class Exercicio_21 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Informe a quantidade de turmas:");
+		int totalTurmas = scan.nextInt();
+		
+		System.out.println("--------------------------------------------");
+		
+		int qtdAlunosTurma = 0;
+		int qtdTotalAlunos = 0;
+		double media;
+		boolean turmaInvalida = true;
+		
+		for (int i = 1; i <= totalTurmas; i++) {
+
+			turmaInvalida = true;
+
+			do {
+				
+				System.out.println("Informe a quantidade de alunos da " + i + "º turma:");
+				qtdAlunosTurma = scan.nextInt();
+				
+				if (qtdAlunosTurma <= 40) {
+					turmaInvalida = false;			
+				} else {
+					
+					System.out.println("Valor inválido! As turmas não podem ter mais de 40 alunos.");
+					System.out.println("-----------------------------------------------------------");
+					
+				}
+				
+			} while (turmaInvalida);
+
+			qtdTotalAlunos += qtdAlunosTurma;
+
+		}
+		
+		media = qtdTotalAlunos / totalTurmas;
+		
+		if(!turmaInvalida) {
+			System.out.println("--------------------------------");
+			System.out.println("Média de alunos por turma: " + media);
+		}
+		
 	}
-
+	
 }
