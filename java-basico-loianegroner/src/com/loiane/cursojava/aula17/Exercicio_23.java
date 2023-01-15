4d31f2b8dc6f971a318a1aa767e4a431563e46ff
denilson-pinto <denilsonpinto89@gmail.com>
2023-01-15 00:32:37 -0300

    Aula 17: exercício 23

--- java-basico-loianegroner/src/com/loiane/cursojava/aula17/Exercicio_23.java
+++ java-basico-loianegroner/src/com/loiane/cursojava/aula17/Exercicio_23.java
@@ -3,7 +3,7 @@ package com.loiane.cursojava.aula17;
 /*
   O Sr. Manoel Joaquim possui uma grande loja de artigos de R$ 1,99,
   com cerca de 10 caixas. Para agilizar o cálculo de quanto cada cliente
-  deve pagar ele desenvolveu um tabela que contém o número de itens
+  deve pagar ele desenvolveu uma tabela que contém o número de itens
   que o cliente comprou e ao lado o valor da conta. Desta forma a
   atendente do caixa precisa apenas contar quantos itens o cliente está
   levando e olhar na tabela de preços. Você foi contratado para
@@ -20,8 +20,13 @@ package com.loiane.cursojava.aula17;
 public class Exercicio_23 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		System.out.println("Lojas Quase Dois - Tabela de preços");
+				
+		for (int i = 1; i <= 50; i++) {
+			System.out.printf("\n" + i + " - R$ %.2f", (i * 1.99));
+		}
+		
 	}
 
 }
